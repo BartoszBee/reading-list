5f0efb4892932a9befd927661c79dff87269e8c0
bartoszb <b.biernacki@delta.poznan.pl>
2025-08-30 09:33:26 +0200

    add styles to body main

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import Header from "@/components/Header";
 export const metadata: Metadata = {
   title: "Reading List",
   description:
-    "Śledź książki, które chcesz przeczytać, jesteś w trakcie czytania lub zakończyłeś czytać",
+    "Prywatny spis Twoich książek.",
 };
 
 export default function RootLayout({
@@ -15,9 +15,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pl">
-      <body>
+      <body className="min-h-dvh bg-gray-200 text-gray-900 antialiased">
         <Header />
-        <main>{children}</main>
+        <main className="mx-auto max-w-5xl px-4 py-6">{children}</main>
       </body>
     </html>
   );
